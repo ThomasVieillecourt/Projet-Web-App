35e813e5affe2c1f7f9e85372010189a11b4d4db
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-06-28 16:13:55 +0200

    Ajout responsive

---
+++ Sofia Meziani Medecin/main.js
@@ -0,0 +1,24 @@
+const btnToggle = document.querySelector('.toggle-btn');
+const btnClicked = document.querySelector('.btn-clicked');
+const body = document.querySelector('body');
+
+const sidebar = document.querySelector('.right-nav');
+
+btnToggle.addEventListener('click', () => {
+
+    on = btnToggle.classList.toggle('btn-clicked');
+
+    if (on == true) {
+        sidebar.style.right = "0px";
+        body.style.overflow = "hidden";
+        sidebar.style.width = 100 + "%";
+        sidebar.style.height = 100 + "vw";
+
+    } else if (on == false) {
+        sidebar.style.right = "-1200px";
+        body.style.overflow = "";
+        sidebar.style.height = "";
+        sidebar.style.width = "";
+    }
+
+})
