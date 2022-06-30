033c73a25561a55bc824441ad21dbf4882b5cca7
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-06-30 08:59:40 +0200

    Finalisation responsive et ajout animations

--- Sofia Meziani Medecin/main.js
+++ Sofia Meziani Medecin/main.js
@@ -1,24 +1,34 @@
-const btnToggle = document.querySelector('.toggle-btn');
-const btnClicked = document.querySelector('.btn-clicked');
-const body = document.querySelector('body');
+const btnToggle = document.querySelector(".toggle-btn");
+const btnClicked = document.querySelector(".btn-clicked");
+const body = document.querySelector("body");
 
-const sidebar = document.querySelector('.right-nav');
+const sidebar = document.querySelector(".right-nav");
 
-btnToggle.addEventListener('click', () => {
+btnToggle.addEventListener("click", () => {
+  on = btnToggle.classList.toggle("btn-clicked");
 
-    on = btnToggle.classList.toggle('btn-clicked');
+  if (on == true) {
+    sidebar.style.right = "0px";
+    body.style.overflow = "hidden";
+    sidebar.style.width = 100 + "%";
+    sidebar.style.height = 100 + "vh";
+  } else if (on == false) {
+    sidebar.style.right = "-1200px";
+    body.style.overflow = "";
+    sidebar.style.height = "";
+    sidebar.style.width = "";
+  }
 
-    if (on == true) {
-        sidebar.style.right = "0px";
-        body.style.overflow = "hidden";
-        sidebar.style.width = 100 + "%";
-        sidebar.style.height = 100 + "vw";
+  console.log(on);
+});
 
-    } else if (on == false) {
-        sidebar.style.right = "-1200px";
-        body.style.overflow = "";
-        sidebar.style.height = "";
-        sidebar.style.width = "";
-    }
+sidebar.addEventListener("click", () => {
+  on = btnToggle.classList.toggle("btn-clicked");
 
-})
+  if (on == false) {
+    sidebar.style.right = "-1200px";
+    body.style.overflow = "";
+    sidebar.style.height = "";
+    sidebar.style.width = "";
+  }
+});
