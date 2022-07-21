7b5c1579d04640f70bab3b52aed30c41e9c329cd
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-07-21 11:48:50 +0200

    Tab and style

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -9,7 +9,8 @@ fetch("https://pokebuildapi.fr/api/v1/pokemon")
 
     pokeName.textContent = pokeData.name;
     pokeImg.src = pokeData.image;
-    pokeId.textContent = pokeData.pokedexId;
+    pokeId.textContent = "#" + pokeData.pokedexId;
+    pokeNameRadar.textContent = pokeData.name;
 
     if (pokeData.apiTypes.length > 1) {
       pokeType.src = pokeData.apiTypes[0].image;
@@ -20,8 +21,7 @@ fetch("https://pokebuildapi.fr/api/v1/pokemon")
     }
 
     if (pokeData.apiPreEvolution != "none") {
-      pokePreEvol.textContent =
-        "Sous évolution : " + pokeData.apiPreEvolution.name;
+      pokePreEvol.textContent = pokeData.apiPreEvolution.name;
       pokePreEvolImg.src =
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
         pokeData.apiPreEvolution.pokedexIdd +
@@ -32,7 +32,7 @@ fetch("https://pokebuildapi.fr/api/v1/pokemon")
     }
 
     if (pokeData.apiEvolutions.length == 1) {
-      pokeEvol.textContent = "Evolution : " + pokeData.apiEvolutions[0].name;
+      pokeEvol.textContent = pokeData.apiEvolutions[0].name;
       pokeEvolImg.src =
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
         pokeData.apiEvolutions[0].pokedexId +
@@ -42,3 +42,19 @@ fetch("https://pokebuildapi.fr/api/v1/pokemon")
       pokeEvolImg.style.display = "none";
     }
   });
+
+function openTab(evt, tabName) {
+  var i, tabContent, tabLinks;
+  tabContent = document.getElementsByClassName("tab");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  tabLinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
