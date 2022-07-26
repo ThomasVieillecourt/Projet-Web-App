e86b4b3a6e9789f3e2d830102462e4289625cf30
Thomas Vieillecourt <thomas.viei@hotmail.fr>
2022-07-26 11:10:25 +0200

    map list tap

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -1,50 +1,77 @@
 // https://pokebuildapi.fr/api/v1/pokemon
+data = [];
+let pokeData = [];
 
-fetch("https://pokebuildapi.fr/api/v1/pokemon")
-  .then((res) => res.json())
-  .then((data) => {
-    const pokeData = data[Math.floor(Math.random() * data.length)];
-    console.log(data);
-    console.log(pokeData);
+async function pokeFetch() {
+  await fetch("https://pokebuildapi.fr/api/v1/pokemon")
+    .then((res) => res.json())
+    .then((data) => (pokeData = data));
 
-    pokeName.textContent = pokeData.name;
-    pokeImg.src = pokeData.image;
-    pokeId.textContent = "#" + pokeData.pokedexId;
-    pokeNameRadar.textContent = pokeData.name;
+  console.log(pokeData);
+  displayFetch();
+}
 
-    if (pokeData.apiTypes.length > 1) {
-      pokeType.src = pokeData.apiTypes[0].image;
-      pokeType2.src = pokeData.apiTypes[1].image;
-    } else {
-      pokeType.src = pokeData.apiTypes[0].image;
-      pokeType2.style.display = "none";
-    }
+pokeFetch();
 
-    if (pokeData.apiPreEvolution != "none") {
-      pokePreEvol.textContent = pokeData.apiPreEvolution.name;
-      pokePreEvolImg.src =
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-        pokeData.apiPreEvolution.pokedexIdd +
-        ".png";
-    } else {
-      pokePreEvol.textContent = "";
-      pokePreEvolImg.style.display = "none";
-    }
+function displayFetch() {
+  const containerCards = document.getElementById("container_cards");
 
-    if (pokeData.apiEvolutions.length == 1) {
-      pokeEvol.textContent = pokeData.apiEvolutions[0].name;
-      pokeEvolImg.src =
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-        pokeData.apiEvolutions[0].pokedexId +
-        ".png";
-    } else {
-      pokeEvol.textContent = "";
-      pokeEvolImg.style.display = "none";
-    }
+  containerCards.innerHTML = pokeData
+    .map(
+      (pokeCard) =>
+        `
+      <div class="card">
+        <img src=${pokeCard.image} alt=${pokeCard.name} id="card_poke">
+        <h2>${pokeCard.name}</h2>
+        <img src="img/bg-${pokeCard.apiTypes[0].name}.png" alt=${pokeCard.name} id="card_bg">
 
-    pokedex.style.background =
-      "url('img/bg-" + pokeData.apiTypes[0].name + ".png') no-repeat top/cover";
-  });
+      </div>
+
+      `
+    )
+
+    .join("");
+
+  pokeData = pokeData[Math.floor(Math.random() * pokeData.length)];
+
+  pokeName.textContent = pokeData.name;
+  pokeImg.src = pokeData.image;
+  pokeId.textContent = "#" + pokeData.pokedexId;
+  pokeNameRadar.textContent = pokeData.name;
+
+  if (pokeData.apiTypes.length > 1) {
+    pokeType.src = pokeData.apiTypes[0].image;
+    pokeType2.src = pokeData.apiTypes[1].image;
+  } else {
+    pokeType.src = pokeData.apiTypes[0].image;
+    pokeType2.style.display = "none";
+  }
+
+  if (pokeData.apiPreEvolution != "none") {
+    pokePreEvol.textContent = pokeData.apiPreEvolution.name;
+    pokePreEvolImg.src =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+      pokeData.apiPreEvolution.pokedexIdd +
+      ".png";
+  } else {
+    pokePreEvol.textContent = "";
+    pokePreEvolImg.style.display = "none";
+  }
+
+  if (pokeData.apiEvolutions.length == 1) {
+    pokeEvol.textContent = pokeData.apiEvolutions[0].name;
+    pokeEvolImg.src =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+      pokeData.apiEvolutions[0].pokedexId +
+      ".png";
+  } else {
+    pokeEvol.textContent = "";
+    pokeEvolImg.style.display = "none";
+  }
+
+  pokedex.style.background =
+    "url('img/bg-" + pokeData.apiTypes[0].name + ".png') no-repeat top/cover";
+}
 
 function openTab(evt, tabName) {
   var i, tabContent, tabLinks;
